d8073580f1a54e58bede9e091bfdc8b113996049
Mark <markcmalloy96@gmail.com>
2022-11-01 15:55:34 +0100

    List items are aligned. The brown and green icons arent showing properly

--- lib/Pages/Search/Components/saved_repositories_listview.dart
+++ lib/Pages/Search/Components/saved_repositories_listview.dart
@@ -19,27 +19,54 @@ class SavedRepositoriesListview extends StatelessWidget {
             style: Theme.of(context).textTheme.headline2,
           ),
         ),
-        Expanded(child: ListView.builder(
-            itemCount: repos.length,
-            padding: const EdgeInsets.symmetric(vertical: 24),
-            itemBuilder: (context, index) {
-              return repoListItem(repos[index]);
-            }))
+        Expanded(
+            child: ListView.builder(
+                itemCount: repos.length,
+                padding: const EdgeInsets.symmetric(vertical: 24),
+                itemBuilder: (context, index) {
+                  return repoListItem(repos[index]);
+                }))
       ],
     );
   }
 
-  repoListItem(Repository repo){
+  repoListItem(Repository repo) {
     return SizedBox(
       height: 60,
       child: Row(
+        mainAxisAlignment: MainAxisAlignment.start,
         children: [
-          SvgPicture.asset(repo.repoAsset),
-          Column(
-            children: [
-              Text(repo.repoName, style: GoogleFonts.roboto(fontWeight: FontWeight.w500, fontSize: 16, color: const Color(0xff333C52)),),
-              Text(repo.repoDescription, style: GoogleFonts.roboto(fontWeight: FontWeight.w400, fontSize: 14, color: const Color(0xff999DA8)),)
-            ],
+          Flexible(
+            flex: 1,
+            child: Container(
+              padding: const EdgeInsets.symmetric(horizontal: 20),
+              child: SvgPicture.asset(repo.repoAsset),
+            ),
+          ),
+          Flexible(
+            flex: 5,
+            child: Column(
+              mainAxisAlignment: MainAxisAlignment.center,
+              crossAxisAlignment: CrossAxisAlignment.start,
+              children: [
+                Text(
+                  repo.repoName,
+                  textAlign: TextAlign.start,
+                  style: GoogleFonts.roboto(
+                      fontWeight: FontWeight.w500,
+                      fontSize: 16,
+                      color: const Color(0xff333C52)),
+                ),
+                Text(
+                  repo.repoDescription,
+                  textAlign: TextAlign.start,
+                  style: GoogleFonts.roboto(
+                      fontWeight: FontWeight.w400,
+                      fontSize: 14,
+                      color: const Color(0xff999DA8)),
+                )
+              ],
+            ),
           )
         ],
       ),
