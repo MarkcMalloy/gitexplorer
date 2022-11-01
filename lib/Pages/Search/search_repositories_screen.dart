0e4b0d022c4f57d770cbda64e6499aa0b7923867
Mark <markcmalloy96@gmail.com>
2022-11-01 01:15:39 +0100

    - Added a textTheme for the Repository library headline - Added the headline text widget in search_repositories_screen.dart

--- lib/Pages/Search/search_repositories_screen.dart
+++ lib/Pages/Search/search_repositories_screen.dart
@@ -1,10 +1,12 @@
 import 'package:flutter/material.dart';
 import 'package:gitexplorer/Services/apiservice.dart';
+
 class SearchRepositoriesScreen extends StatefulWidget {
   const SearchRepositoriesScreen({Key? key}) : super(key: key);
 
   @override
-  State<SearchRepositoriesScreen> createState() => _SearchRepositoriesScreenState();
+  State<SearchRepositoriesScreen> createState() =>
+      _SearchRepositoriesScreenState();
 }
 
 class _SearchRepositoriesScreenState extends State<SearchRepositoriesScreen> {
@@ -16,14 +18,25 @@ class _SearchRepositoriesScreenState extends State<SearchRepositoriesScreen> {
     getSavedRepositoried();
   }
 
-  Future getSavedRepositoried() async {
-  }
+  Future getSavedRepositoried() async {}
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      body: Column(
+      body: SafeArea(
+        child: Column(
+          children: [screenHeader()],
+        ),
+      ),
+    );
+  }
 
+  Widget screenHeader() {
+    return Padding(
+      padding: EdgeInsets.only(left: 12, top: 12),
+      child: Text(
+        "Repository library",
+        style: Theme.of(context).textTheme.headline1,
       ),
     );
   }
--- lib/main.dart
+++ lib/main.dart
@@ -1,5 +1,6 @@
 import 'package:flutter/material.dart';
-import 'package:gitexplorer/Pages/search_repositories_screen.dart';
+import 'package:gitexplorer/Pages/Search/search_repositories_screen.dart';
+import 'package:google_fonts/google_fonts.dart';
 
 void main() {
   runApp(const MyApp());
@@ -11,11 +12,15 @@ class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      debugShowCheckedModeBanner: false,
-      theme: ThemeData(
-        backgroundColor: Colors.white
-      ),
-      home: const SearchRepositoriesScreen()
-    );
+        debugShowCheckedModeBanner: false,
+        theme: ThemeData(
+            textTheme: TextTheme(
+              headline1: GoogleFonts.roboto(
+                  color: Color(0xff333C52),
+                  fontWeight: FontWeight.w500,
+                  fontSize: 20), // Petrol color from figma
+            ),
+            backgroundColor: Colors.white),
+        home: const SearchRepositoriesScreen());
   }
 }
